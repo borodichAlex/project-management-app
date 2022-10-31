62f6521584f0ac53871c278bcbe6f437f0041511
Aliaksandr Barodzich <borodichalexander97@gmail.com>
2022-10-31 16:41:15 +0300

    [feat]PMA-24: Include translate module in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NgxTranslateModule } from './translate/translate.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxTranslateModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
