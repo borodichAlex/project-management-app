78df19cf65bbe7c760de29105e9886ed90ee2c2b
Pavel Zabalotny <pavelzabalotny@gmail.com>
2022-11-02 21:49:16 +0300

    [feat]PMA-23: implement Angular Material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './core/material/material.module';
 
 import { NgxTranslateModule } from './translate/translate.module';
 
@@ -14,6 +15,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     NgxTranslateModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/core/material/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [],
+  exports: [MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule],
+})
+export class MaterialModule {}
