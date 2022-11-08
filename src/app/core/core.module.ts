f751cf6ca1479629e46a599f0d268077636764d3
BlueOwll <blueowl.n@gmail.com>
2022-11-09 00:45:47 +0300

    [feat]PMA-46: add language toggle component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { UserTokenService } from './services/user-token.service';
+import { LanguageTogglerComponent } from './language-toggler/language-toggler.component';
 
 @NgModule({
-  declarations: [NotFoundComponent],
-  imports: [CommonModule],
+  declarations: [NotFoundComponent, LanguageTogglerComponent],
+  imports: [
+    CommonModule,
+    MatButtonToggleModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [UserTokenService],
+  exports: [LanguageTogglerComponent],
 })
 export class CoreModule {}
