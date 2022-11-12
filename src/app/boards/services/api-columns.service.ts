da71e57ce0d98b45c43939772b19782cb2d4749f
Pavel Zabalotny <pavelzabalotny@gmail.com>
2022-11-13 00:06:54 +0300

    [feat]PMA-51: get columns with tasks

--- src/app/boards/services/api-columns.service.ts
+++ src/app/boards/services/api-columns.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { columnApi } from '../../shared/constants';
-import { TColumn, TNewColumn } from '../interfaces/column.interface';
+import {
+  IColumnFull,
+  TColumn,
+  TNewColumn,
+} from '../interfaces/column.interface';
 
 @Injectable()
 export class ApiColumnsService {
@@ -11,11 +16,18 @@ export class ApiColumnsService {
     return this.http.get<TColumn[]>(columnApi(boardId));
   }
 
-  public create(column: TNewColumn, boardId: string) {
+  public getAllById(
+    boardId: string,
+    columnId: string,
+  ): Observable<IColumnFull> {
+    return this.http.get<IColumnFull>(columnApi(boardId, columnId));
+  }
+
+  public create(boardId: string, column: TNewColumn) {
     return this.http.post<TColumn>(columnApi(boardId), column);
   }
 
-  public delete(columnId: string, boardId: string) {
+  public delete(boardId: string, columnId: string) {
     return this.http.delete(columnApi(boardId, columnId));
   }
 }
--- src/app/boards/services/columns.service.ts
+++ src/app/boards/services/columns.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { TColumn, TNewColumn } from '../interfaces/column.interface';
+import { BehaviorSubject, forkJoin, mergeMap, Observable } from 'rxjs';
+import {
+  IColumnFull,
+  TColumn,
+  TNewColumn,
+} from '../interfaces/column.interface';
 import { ApiColumnsService } from './api-columns.service';
 
 @Injectable()
@@ -13,10 +17,22 @@ export class ColumnsService {
 
   public loadAll(boardId: string): void {
     this.isLoading$.next(true);
-    this.apiColumns.getAll(boardId).subscribe((columns) => {
-      this.columns$.next(columns);
-      this.isLoading$.next(false);
-    });
+    this.apiColumns
+      .getAll(boardId)
+      .pipe(
+        mergeMap((column) => {
+          const arr: Observable<IColumnFull>[] = column.map((item) =>
+            this.apiColumns.getAllById(boardId, item.id),
+          );
+          return forkJoin(arr);
+        }),
+      )
+      .subscribe((columns: IColumnFull[]) => {
+        // eslint-disable-next-line no-console
+        console.log('columns:', columns);
+        this.columns$.next(columns);
+        this.isLoading$.next(false);
+      });
   }
 
   public get columns(): Observable<TColumn[]> {
@@ -28,14 +44,14 @@ export class ColumnsService {
   }
 
   public create(column: TNewColumn, boardId: string): void {
-    this.apiColumns.create(column, boardId).subscribe((newColumn) => {
+    this.apiColumns.create(boardId, column).subscribe((newColumn) => {
       const newColumns: TColumn[] = [...this.columns$.value, newColumn];
       this.columns$.next(newColumns);
     });
   }
 
   public delete(columnId: string, boardId: string): void {
-    this.apiColumns.delete(columnId, boardId).subscribe(() => {
+    this.apiColumns.delete(boardId, columnId).subscribe(() => {
       const newColumns = this.columns$.value.filter(
         (column) => column.id !== columnId,
       );
