a3c86fac2c66b0bcee83bfde9548b20c8c3a4ed0
BlueOwll <blueowl.n@gmail.com>
2022-11-12 21:07:39 +0300

    [feat]PMA-14: add footer

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-footer',
@@ -8,5 +10,21 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 })
 export class FooterComponent {
   // eslint-disable-next-line @typescript-eslint/no-useless-constructor, @typescript-eslint/no-empty-function
-  constructor() {}
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'git-logo',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        '../../../../assets/icons/github-logo.svg',
+      ),
+    );
+    this.matIconRegistry.addSvgIcon(
+      'rss-logo',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        '../../../../assets/icons/rss.svg',
+      ),
+    );
+  }
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,7 +5,9 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { TranslateModule } from '@ngx-translate/core';
 import { ErrorHandlerInterceptor } from './interceptors/error-handler.interceptor';
 import { UserStateService } from './services/user-state.service';
@@ -13,6 +15,7 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
 import { LanguageTogglerComponent } from './components/language-toggler/language-toggler.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +23,7 @@ import { HeaderComponent } from './components/header/header.component';
     LanguageTogglerComponent,
     HeaderComponent,
     ErrorDialogComponent,
+    FooterComponent,
   ],
   imports: [
     CommonModule,
@@ -28,8 +32,10 @@ import { HeaderComponent } from './components/header/header.component';
     MatButtonModule,
     MatMenuModule,
     FormsModule,
+    MatTooltipModule,
     ReactiveFormsModule,
     TranslateModule,
+    MatIconModule,
   ],
   providers: [
     UserStateService,
@@ -39,6 +45,6 @@ import { HeaderComponent } from './components/header/header.component';
       multi: true,
     },
   ],
-  exports: [LanguageTogglerComponent, HeaderComponent],
+  exports: [LanguageTogglerComponent, HeaderComponent, FooterComponent],
 })
 export class CoreModule {}
