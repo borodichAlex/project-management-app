fed9634c68940f7c54a9a84616b4fa5bc09d1e4d
BlueOwll <blueowl.n@gmail.com>
2022-11-14 21:42:44 +0300

    [feat]PMA-46: leave header visible permanently

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/services/auth.service';
+import { RoutePaths } from 'src/app/shared/constants';
 import { UserStateService } from '../../services/user-state.service';
 import { UserTokenService } from '../../services/user-token.service';
 
@@ -16,11 +18,20 @@ export class HeaderComponent implements OnInit {
     public userToken: UserTokenService,
     private auth: AuthService,
     private userState: UserStateService,
+    private router: Router,
   ) {}
 
   public ngOnInit(): void {
     // this.userName = this.userState.user.name;
     this.userName = '@USERNAME';
+  }
+
+  public onLogInClick() {
+    this.router.navigate([RoutePaths.authPrefix + RoutePaths.login]);
+  }
+
+  public onSignUpClick() {
+    this.router.navigate([RoutePaths.authPrefix + RoutePaths.signup]);
   }
 
   public onEditProfileClick() {
