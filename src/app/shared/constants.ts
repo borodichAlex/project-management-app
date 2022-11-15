e0acfd81b9b766e5cc0fd827ca92ad12e7394673
Pavel Zabalotny <pavelzabalotny@gmail.com>
2022-11-15 23:50:21 +0300

    [refactor]PMA-66: move api functions to utils

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -17,19 +17,3 @@ export enum RoutePaths {
   login = 'login',
   signup = 'signup',
 }
-
-export function columnApi(boardId: string, columnId?: string): string {
-  return columnId
-    ? `${API_BOARDS}/${boardId}/${RoutePaths.columns}/${columnId}`
-    : `${API_BOARDS}/${boardId}/${RoutePaths.columns}`;
-}
-
-export function taskApi(
-  boardId: string,
-  columnId: string,
-  taskId?: string,
-): string {
-  return taskId
-    ? `${columnApi(boardId, columnId)}/tasks/${taskId}`
-    : `${columnApi(boardId, columnId)}/tasks`;
-}
