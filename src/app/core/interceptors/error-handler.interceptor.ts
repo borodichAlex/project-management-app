fcc36ba4bcdb5307f332be1c70a9c2690a07e0aa
BlueOwll <blueowl.n@gmail.com>
2022-11-25 00:26:30 +0300

    [feat]PMA-81: add no answer error handling

--- src/app/core/interceptors/error-handler.interceptor.ts
+++ src/app/core/interceptors/error-handler.interceptor.ts
@@ -8,6 +8,7 @@ import {
 } from '@angular/common/http';
 import { EMPTY, Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { GATEWAY_TIMEOUT_ERROR } from 'src/app/shared/constants';
 import { ErrorDialogService } from '../services/error-dialog.service';
 import { IData } from '../services/types';
 
@@ -21,10 +22,21 @@ export class ErrorHandlerInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
-        const data: IData = {
-          reason: error && error.error.message ? error.error.message : '',
-          status: error.status,
-        };
+        let data: IData;
+        if (error && error.status === GATEWAY_TIMEOUT_ERROR) {
+          data = {
+            reason: 'No answer from server',
+            status: error.status,
+          };
+        } else {
+          data = {
+            reason:
+              error && error.error.message
+                ? error.error.message
+                : 'Unknown error',
+            status: error.status,
+          };
+        }
         this.errorDialogService.openDialog(data);
         return EMPTY;
       }),
